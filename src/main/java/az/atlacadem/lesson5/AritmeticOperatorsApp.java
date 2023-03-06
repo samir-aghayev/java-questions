eee9f93eb20930556fc6523da52db22a07773582
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-06 19:41:51 +0400

    print examples attached

--- src/main/java/az/atlacadem/lesson5/AritmeticOperatorsApp.java
+++ src/main/java/az/atlacadem/lesson5/AritmeticOperatorsApp.java
@@ -6,20 +6,31 @@ public class AritmeticOperatorsApp {
         int b = 3;
         int c = a + b;
         System.out.println(c);
+
         int d = a - b;
         System.out.println(d);
+
         int e = a * b;
         System.out.println(e);
+
         int f = a / b;
         System.out.println(f);
+
         double g = (double) a / b;
+        System.out.println(g);
+
         int r = a % b;
+        System.out.println(r);
+
         a++;
         System.out.println(a);
+
         b++;
         System.out.println(b);
+
         ++a;
         System.out.println(a);
+
         ++b;
         System.out.println(b);
     }
