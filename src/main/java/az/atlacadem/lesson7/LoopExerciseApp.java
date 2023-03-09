2105261e25ccf3679415690bee561493f2478311
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-09 20:11:28 +0400

    print examples attached

--- src/main/java/az/atlacadem/lesson7/LoopExerciseApp.java
+++ src/main/java/az/atlacadem/lesson7/LoopExerciseApp.java
@@ -6,10 +6,10 @@ public class LoopExerciseApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        int sum=0;
-        for (int i = 0; i <4; i++) {
-            sum+=(num%10);
-            num/=10;
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            sum += (num % 10);
+            num /= 10;
         }
         System.out.println(sum);
     }
