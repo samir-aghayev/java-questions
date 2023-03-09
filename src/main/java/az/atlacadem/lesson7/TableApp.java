96e1792068da61d0cafae57f9d66d7317d2c1bf3
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-09 20:17:28 +0400

    print examples attached

---
+++ src/main/java/az/atlacadem/lesson7/TableApp.java
@@ -0,0 +1,9 @@
+package az.atlacadem.lesson7;
+
+public class TableApp {
+    public static void main(String[] args) {
+        for (int i = 65; i < 91; i++) {
+            System.out.println((char) (i));
+        }
+    }
+}
