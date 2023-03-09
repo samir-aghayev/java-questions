15a5174dd278dad246594ad10a2501272d52d08f
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-09 19:11:58 +0400

    print examples attached

--- src/main/java/az/atlacadem/lesson7/FizzBUzz.java
+++ src/main/java/az/atlacadem/lesson7/FizzBUzz.java
@@ -8,9 +8,9 @@ public class FizzBUzz {
         int n= scanner.nextInt();
         if(n%3==0 && n%5==0){
             System.out.println("Fizzbuzz");
-        }else if(n%3==0 && n%5!=0){
+        }else if(n % 3 == 0){
             System.out.println("Fizz");
-        }else if(n%3!=0 && n%5==0){
+        }else if(n % 5 == 0){
             System.out.println("Buzz");
         }else{
             System.out.println(n);
