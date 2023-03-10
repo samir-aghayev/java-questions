4d690a5ec024029d9c5b119bb04e469197516f60
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-10 19:29:37 +0400

    print examples attached

---
+++ src/main/java/az/atlacadem/lesson8/WhileExercise2.java
@@ -0,0 +1,18 @@
+package az.atlacadem.lesson8;
+
+import java.util.Scanner;
+
+public class WhileExercise2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int sum = 0;
+        while (num > 0) {
+            if ((num % 10) % 2 == 0) {
+                sum += num % 10;
+            }
+            num /= 10;
+        }
+        System.out.println(sum);
+    }
+}
