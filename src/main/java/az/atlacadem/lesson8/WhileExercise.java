6e9346aa17da81a5e42cb1cda91cdadbfd96aae7
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-10 20:25:26 +0400

    print examples attached

--- src/main/java/az/atlacadem/lesson8/WhileExercise.java
+++ src/main/java/az/atlacadem/lesson8/WhileExercise.java
@@ -6,11 +6,11 @@ public class WhileExercise {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        int count = 0;
+        int say = 0;
         while (num > 0) {
-            count++;
+            say++;
             num /= 10;
         }
-        System.out.println(count);
+        System.out.println(say);
     }
 }
