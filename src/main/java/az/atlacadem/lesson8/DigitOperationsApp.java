87947d153ce41891180256cc14706ecc5a6cc30c
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-10 19:48:45 +0400

    print examples attached

--- src/main/java/az/atlacadem/lesson8/DigitOperationsApp.java
+++ src/main/java/az/atlacadem/lesson8/DigitOperationsApp.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class DigitOperationsApp {
     public static void main(String[] args) {
+        //1. input
         int num = new Scanner(System.in).nextInt();
+        //2. process
         int sum = 0;
         int prod = 1;
         int count = 0;
@@ -15,6 +17,7 @@ public class DigitOperationsApp {
             sum += rem;
             prod *= rem;
         }
+        //3. output
         System.out.printf("Sum of digits is: %d\n", sum);
         System.out.printf("Prod of digits is: %d\n", prod);
         System.out.printf("Count of digits is: %d\n", count);
