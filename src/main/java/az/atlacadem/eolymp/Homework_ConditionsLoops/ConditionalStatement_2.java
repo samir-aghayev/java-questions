8c4a2195a69bcb1b274d8e12cb2725298520549f
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-13 20:32:27 +0400

    print examples attached

--- src/main/java/az/atlacadem/eolymp/Homework_ConditionsLoops/ConditionalStatement_2.java
+++ src/main/java/az/atlacadem/eolymp/Homework_ConditionsLoops/ConditionalStatement_2.java
@@ -2,15 +2,15 @@ package az.atlacadem.eolymp.Homework_ConditionsLoops;
 
 import java.util.Scanner;
 
-public class ConditionalStatement_1 {
+public class ConditionalStatement_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
         int y = 0;
-        if (x < 5) {
-            y = x * x - 3 * x + 4;
+        if (x >= 10) {
+            y = (x * x * x) + (5 * x);
         } else {
-            y = x + 7;
+            y = (x * x) - (2 * x) + 4;
         }
         System.out.println(y);
     }
