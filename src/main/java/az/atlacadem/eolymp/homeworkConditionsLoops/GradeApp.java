d15f59967b890ce15068fdd23da609d9f324751e
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-14 19:41:02 +0400

    new commit

---
+++ src/main/java/az/atlacadem/eolymp/homeworkConditionsLoops/GradeApp.java
@@ -0,0 +1,11 @@
+package az.atlacadem.eolymp.homeworkConditionsLoops;
+
+import java.util.Scanner;
+
+public class GradeApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = scanner.nextDouble();
+        System.out.printf("%.2f", grade);
+    }
+}
