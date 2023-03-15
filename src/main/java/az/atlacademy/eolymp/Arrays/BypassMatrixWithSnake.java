b2160e12c957248370b8fc8cae96c368392f2dbe
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-15 22:42:01 +0400

    new commit

--- src/main/java/az/atlacademy/eolymp/Arrays/BypassMatrixWithSnake.java
+++ src/main/java/az/atlacademy/eolymp/Arrays/BypassMatrixWithSnake.java
@@ -1,4 +1,32 @@
 package az.atlacademy.eolymp.Arrays;
 
+import java.util.Scanner;
+
 public class BypassMatrixWithSnake {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] a = new int[n][n];
+        int num = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = num;
+                    num++;
+                }
+            } else {
+                for (int j = n - 1; j >= 0; j--) {
+                    a[i][j] = num;
+                    num++;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <n ; j++) {
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
