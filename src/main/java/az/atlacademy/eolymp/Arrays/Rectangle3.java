39662d1e30c465c2fa5bd117c364bcc2b2a0f7a6
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-15 23:30:59 +0400

    new commit

---
+++ src/main/java/az/atlacademy/eolymp/Arrays/Rectangle3.java
@@ -0,0 +1,18 @@
+package az.atlacademy.eolymp.Arrays;
+
+import java.util.Scanner;
+
+public class Rectangle3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <m ; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
