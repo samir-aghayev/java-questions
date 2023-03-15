b76a3a68b918d47b2dabc0dcf98a6945d8f44d87
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-15 20:25:56 +0400

    new commit

--- src/main/java/az/atlacademy/eolymp/homeworkConditionsLoops/AtLeastOneConditionTutOfTwo.java
+++ src/main/java/az/atlacademy/eolymp/homeworkConditionsLoops/AtLeastOneConditionTutOfTwo.java
@@ -1,4 +1,15 @@
 package az.atlacademy.eolymp.homeworkConditionsLoops;
 
+import java.util.Scanner;
+
 public class AtLeastOneConditionTutOfTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n % 2 != 0 ||  (n >99 && n <1000)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
 }
