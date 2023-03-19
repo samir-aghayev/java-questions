98d2be79ea5308ea0f1230652d14952d195738c8
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-19 20:49:26 +0400

    new commit

---
+++ src/main/java/az/atlacademy/eolymp/preExamGeneralMixedHomework/TheLastDigit.java
@@ -0,0 +1,11 @@
+package az.atlacademy.eolymp.preExamGeneralMixedHomework;
+
+import java.util.Scanner;
+
+public class TheLastDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(n % 10);
+    }
+}
