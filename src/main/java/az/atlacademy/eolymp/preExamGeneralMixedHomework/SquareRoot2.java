25bb6231f3b550cea9f5b7b82f97d7f9a4a47535
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-22 14:48:56 +0400

    new commit

---
+++ src/main/java/az/atlacademy/eolymp/preExamGeneralMixedHomework/SquareRoot2.java
@@ -0,0 +1,11 @@
+package az.atlacademy.eolymp.preExamGeneralMixedHomework;
+
+import java.util.Scanner;
+
+public class SquareRoot2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        System.out.printf("%.6f", Math.sqrt(n));
+    }
+}
