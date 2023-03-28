c81a7e774ef85eb5443fa6e42d5a25b08dfd7e37
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-28 21:50:45 +0400

    new commit

--- src/main/java/az/atlacademy/lesson9/Method.java
+++ src/main/java/az/atlacademy/lesson9/Method.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Method {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int n=scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         printDigit(n);
     }
 
