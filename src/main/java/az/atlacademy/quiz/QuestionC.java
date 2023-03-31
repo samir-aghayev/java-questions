06854892edd9e0813c92efad874bbc3aff22cabc
Ağayev Samir <agayev.samir8@gmail.com>
2023-03-31 20:28:52 +0400

    question C

---
+++ src/main/java/az/atlacademy/quiz/QuestionC.java
@@ -0,0 +1,24 @@
+package az.atlacademy.quiz;
+
+import java.util.Scanner;
+
+public class QuestionC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
