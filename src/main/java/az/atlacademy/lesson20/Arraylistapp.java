1cfb826b2ecf74c20af9ca1025fa12e5c698ff1e
Ağayev Samir <agayev.samir8@gmail.com>
2023-05-11 19:51:42 +0400

    Arraylist

--- src/main/java/az/atlacademy/lesson20/Arraylistapp.java
+++ src/main/java/az/atlacademy/lesson20/Arraylistapp.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 
 public class Arraylistapp {
     public static void main(String[] args) {
-        ArrayList<Integer> num=new ArrayList<>();
-        for (int i = 0; i <10 ; i++) {
-            num.add((int)(Math.random()*100));
+        ArrayList<Integer> num = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            num.add((int) (Math.random() * 100));
         }
         System.out.println(num);
 
         num.sort(Integer::compareTo);
         System.out.println(num);
 
-        num.add(num.size()-1, 15);
+        num.add(num.size() - 1, 15);
         System.out.println(num);
 
         System.out.println(num.contains(15));
