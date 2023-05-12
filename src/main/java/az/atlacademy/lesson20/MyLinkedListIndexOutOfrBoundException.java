0bdffb53b1ab951e2dd5cbbf8a591e0b3d5851a2
Ağayev Samir <agayev.samir8@gmail.com>
2023-05-12 19:37:56 +0400

    Arraylist

---
+++ src/main/java/az/atlacademy/lesson20/MyLinkedListIndexOutOfrBoundException.java
@@ -0,0 +1,7 @@
+package az.atlacademy.lesson20;
+
+public class MyLinkedListIndexOutOfrBoundException extends IndexOutOfBoundsException{
+    public MyLinkedListIndexOutOfrBoundException(String message) {
+        super(message);
+    }
+}
