daea124f4b7c3724b10be4f1c78d03e8703c3a04
Ağayev Samir <agayev.samir8@gmail.com>
2023-05-16 20:21:29 +0400

    MyOrderClassAdded

--- src/main/java/az/atlacademy/lesson22/stack/OperationEnum.java
+++ src/main/java/az/atlacademy/lesson22/stack/OperationEnum.java
@@ -1,7 +1,11 @@
 package az.atlacademy.lesson22.stack;
 
 public enum OperationEnum {
-    Undo, Redo, Delete, Home, End, Enter, Reneme;
-
-
+    UNDO,
+    REDO,
+    DELETE,
+    HOME,
+    END,
+    ENTER,
+    RENEME
 }
