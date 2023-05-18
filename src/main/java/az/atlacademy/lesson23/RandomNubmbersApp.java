581c704db43fef4846a3ba1a8b21f4b1e5873ae8
Ağayev Samir <agayev.samir8@gmail.com>
2023-05-18 19:03:12 +0400

    MyOrderClassAdded

---
+++ src/main/java/az/atlacademy/lesson23/RandomNubmbersApp.java
@@ -0,0 +1,25 @@
+package az.atlacademy.lesson23;
+
+public class RandomNubmbersApp {
+    public static void main(String[] args) {
+        int[] nums = new int[20];
+        int max = 0;
+        int min = -1;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * 100);
+            if (max < nums[i]) {
+                max = nums[i];
+            }
+            if (min > nums[i]) {
+                min = nums[i];
+            }
+            sum += nums[i];
+        }
+        double average = sum / 20.0;
+        System.out.println("Max number: " + max);
+        System.out.println("Min number: " + min);
+        System.out.println("Numbers average: " + average);
+    }
+
+}
