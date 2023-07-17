9844cd2746baa4a69201eb62397fc440fe21b8a6
Ağayev Samir <agayev.samir8@gmail.com>
2023-07-17 20:47:30 +0400

    JDBC

--- src/main/java/az/atlacademy/jdbc_ders/JDBCApp.java
+++ src/main/java/az/atlacademy/jdbc_ders/JDBCApp.java
@@ -7,9 +7,27 @@ import java.util.List;
 
 public class JDBCApp {
     public static final String getAllCustomers = "SELECT * FROM Customers;";
+    public static final String updateCustomerSql = "UPDATE Customers SET first_name=? where first_name=?;";
 
     public static void main(String[] args) {
+        updateCustomers("David", "ILqar").forEach(System.out::println);
         getAllCustomers().forEach(System.out::println);
+    }
+
+    private static List<Customers> updateCustomers(String set, String values) {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/homework",
+                "postgres",
+                "postgres")) {
+            PreparedStatement query = conn.prepareStatement(updateCustomerSql);
+            query.setString(1, values);
+            query.setString(2, set);
+            int affectRows = query.executeUpdate();
+            System.out.println(affectRows);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
     }
 
     private static List<Customers> getAllCustomers() {
@@ -19,13 +37,13 @@ public class JDBCApp {
                 "postgres")) {
             PreparedStatement query = conn.prepareStatement(getAllCustomers);
             ResultSet resultSet = query.executeQuery();
-            List<Customers> customers=new ArrayList<>();
-            while (resultSet.next()){
-                String first_name=resultSet.getString("first_name");
-                String last_name=resultSet.getString("last_name");
-                String address=resultSet.getString("address");
-                String email=resultSet.getString("email");
-                customers.add(new Customers(first_name,last_name,address, email));
+            List<Customers> customers = new ArrayList<>();
+            while (resultSet.next()) {
+                String first_name = resultSet.getString("first_name");
+                String last_name = resultSet.getString("last_name");
+                String address = resultSet.getString("address");
+                String email = resultSet.getString("email");
+                customers.add(new Customers(first_name, last_name, address, email));
             }
             return customers;
         } catch (SQLException e) {
